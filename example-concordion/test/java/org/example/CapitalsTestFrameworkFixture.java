20a3f532e0f1afe8a5967a85dc7fa9ac755b1e00
Cedric Van Pelt <cedric@bluetrails.nl>
2024-11-04 22:28:58 +0100

    test in a specific folder

--- example-concordion/test/java/org/example/CapitalsTestFrameworkFixture.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class CapitalsTestFrameworkFixture {
-    public String getCapital(String country){
-        Capitals cap = new Capitals();
-        return cap.getCapital(country);
-    }
-}
